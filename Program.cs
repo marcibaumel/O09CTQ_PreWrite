bafdbb0f11d543c128b775d4b90cb391077f8bed
Marthon <marcibaumel@gmail.com>
2021-04-25 20:00:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace O09CTQ_PreWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
