ee9b66685444f4a26ede4be7747b6eabf6bede8d
Marthon <marcibaumel@gmail.com>
2021-04-30 09:36:01 +0200

    tests

---
+++ O09CTQ_PreWrite.UTest/ComparerTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using O09CTQ_PreWrite.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static O09CTQ_PreWrite.Implementation.Comparer;
+using static O09CTQ_PreWrite.Implementation.Libary;
+
+namespace O09CTQ_PreWrite.UTest
+{
+    [TestFixture]
+    public class ComparerTest
+    {
+
+        [Test]
+        public void CompareTest_ReciveTwoBooks_GiveBacksInOrder()
+        {
+            
+            bookTest1 = new Book("", "", 2);
+            bookTest2 = new Book("", "", 5);
+            List<Book> listTest = new List<Book>();
+            listTest.Add(bookTest1);
+            listTest.Add(bookTest2);
+
+            listTest.Sort(new DescendingComparer());    
+
+
+            Assert.IsTrue(listTest.First().Equals(bookTest2));
+        }
+
+        private Book bookTest1;
+        private Book bookTest2;
+    }
+}
---
+++ O09CTQ_PreWrite.UTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("O09CTQ_PreWrite.UTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("O09CTQ_PreWrite.UTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1e1e2d2e-108f-4b64-a15e-4a7bd54e24c1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
